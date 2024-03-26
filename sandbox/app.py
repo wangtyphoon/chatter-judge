8949aef63b5f22695058cd659c8736dd4e601ba2
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2024-03-26 19:41:15 +0800

    fix: TLE detection

--- sandbox/app.py
+++ sandbox/app.py
@@ -36,29 +36,32 @@ def run(run_code: RunCode, background_tasks: BackgroundTasks):
     with open(temp_file, "w") as f:
         f.write(run_code.code)
 
+    proc = subprocess.Popen(
+        [
+            "nsjail",
+            "-Q",
+            "--config",
+            "/home/user/nsjail.cfg",
+            "-R",
+            f"{temp_file}:/home/user/run.py",
+            "--",
+            "/usr/bin/python3",
+            "/home/user/run.py",
+        ],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
     try:
-        p = subprocess.run(
-            [
-                "nsjail",
-                "-Q",
-                "--config",
-                "/home/user/nsjail.cfg",
-                "-R",
-                f"{temp_file}:/home/user/run.py",
-                "--",
-                "/usr/bin/python3",
-                "/home/user/run.py",
-            ],
-            input=run_code.input.encode(),
-            capture_output=True,
-            timeout=TIMEOUT,
-        )
+        stdout, stderr = proc.communicate(run_code.input.encode(), timeout=TIMEOUT)
     except subprocess.TimeoutExpired:
+        proc.kill()
         os.remove(temp_file)
-        return Result.TIME_LIMIT_EXCEED, ""
+        return {"status": Result.TIME_LIMIT_EXCEED, "msg": ""}
 
     background_tasks.add_task(os.remove, temp_file)
 
-    if p.returncode == 0:
-        return {"status": Result.SUCCESS, "msg": p.stdout.hex()}
-    return {"status": Result.RUNTIME_ERROR, "msg": p.stderr.hex()}
+    if proc.returncode == 0:
+        return {"status": Result.SUCCESS, "msg": stdout.hex()}
+    return {"status": Result.RUNTIME_ERROR, "msg": stderr.hex()}
