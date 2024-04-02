20eb42ce452238b13a67b36730c25b6bfa5de4e0
wangtyphoon <mfw9889@gmail.com>
2024-04-02 19:01:50 +0800

    build method change with token.json

--- web/Chatter/ChatBot/load_creds.py
+++ web/Chatter/ChatBot/load_creds.py
@@ -1,3 +1,4 @@
+import os
 import os.path
 
 from google.auth.transport.requests import Request
@@ -5,6 +6,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 SCOPES = ['https://www.googleapis.com/auth/generative-language.tuning']
+TOKEN_PATH = "./token.json"
 
 def load_creds():
     """Converts `client_secret.json` to a credential object.
@@ -16,10 +18,11 @@ def load_creds():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('Chatter/ChatBot/token.json'):
-        creds = Credentials.from_authorized_user_file('Chatter/ChatBot/token.json', SCOPES)
+    if os.path.exists(TOKEN_PATH):
+        creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
+        raise ValueError("bla bla bla")
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
@@ -27,6 +30,6 @@ def load_creds():
                 'Chatter/ChatBot/client_secret.json', SCOPES)
             creds = flow.run_local_server()
         # Save the credentials for the next run
-        with open('Chatter/ChatBot/token.json', 'w') as token:
+        with open(TOKEN_PATH, 'w') as token:
             token.write(creds.to_json())
     return creds
